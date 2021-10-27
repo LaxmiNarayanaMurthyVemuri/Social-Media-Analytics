02c4a9763e9ec37f945e010289c2a072103aa3c2
Kondaspoorthy <kondasaispoorthy@msitprogram.net>
2021-10-27 09:47:24 +0530

    Generate DataFrames

---
+++ pandas2.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as np
+cart={"product":["Mobile","Laptop","Washingmachine","car"],"price":[20000,45000,47000,120000],"Type":["electriconics","elect","cleaning","Machine"]}
+df=pd.DataFrame(cart)
+varname=df.loc[df["price"]>25000]
+print(varname)
