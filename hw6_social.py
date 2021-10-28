e15877d9638078a36eca72782d687efa61b8f7ff
Pratyush-98 <pratyushsharma899@msitprogram.net>
2021-10-28 11:19:06 +0530

    completed making data frame

--- hw6_social.py
+++ hw6_social.py
@@ -4,6 +4,7 @@ Name:
 Roll Number:
 """
 
+from pandas.core.frame import DataFrame
 import hw6_social_tests as test
 
 project = "Social" # don't edit this
@@ -25,7 +26,8 @@ Parameters: str
 Returns: dataframe
 '''
 def makeDataFrame(filename):
-    return
+    df=pd.read_csv(filename)
+    return df
 
 
 '''
